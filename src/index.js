b16d72756690801209e2f7335b70cc8db0976f97
svanti17 <svanti17@yandex.ru>
2024-08-19 20:44:26 +0300

    Write code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let strNumber = (Math.abs(n)).toString();
+    let result = "";
+    let char = "";
+
+    for (let i = 0; i < strNumber.length; i++) {
+        char = strNumber[strNumber.length - (i + 1)];
+        result = `${result}${char}`;
+    }
+    return result;
 }
